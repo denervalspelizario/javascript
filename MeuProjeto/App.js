cf6c6226346ea2e013ad9505575431860918f7d8
denervalspelizario <denervalspelizario@gmail.com>
2022-06-12 11:49:29 -0300

    novo app

--- MeuProjeto/App.js
+++ MeuProjeto/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Meu primeiro app!</Text>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#03a9f4',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ MeuProjeto/meu-aplicativo/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Stylesheet,Text } from 'react-native';
+
+export default function App() {
+  return  (
+      <Text>Hello World!</Text>   
+  );
+
+
+}
+
+const estilos = StyleSheet .create({
+
+
+
+
+});
